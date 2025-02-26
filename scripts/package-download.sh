61728f25c483e0ff01ace2019c5067e62a9f2ff2
JasonSun0593 <srm2020@126.com>
2025-02-26 17:26:22 +0800

    chore: try removing unzip command

--- scripts/package-download.sh
+++ scripts/package-download.sh
@@ -16,9 +16,3 @@ mkdir -p "$PKG"
 echo "Fetching Qt_Static-6.6.0 package"
 gh release download --repo "$QT_STAT_REPO" "$QT_STAT_VERSION" \
     --pattern "$QT_STAT_ASSET" --dir "$QT_STAT_PKG"
-	
-echo "Unzipping component package \"$QT_STAT_ASSET\""
-unzip '*.zip' -d "$QT_STAT_PKG"
-
-echo "Remove component package ZIP"
-rm "*.zip" -rf
