7ef08cc6fec7d5f353b41a2e1b6102ff3fb9dabb
JasonSun0593 <srm2020@126.com>
2025-02-26 14:22:00 +0800

    fix: try to take lines out of bat file again

--- scripts/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-c:
-cd C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/BuildTools/VC/Auxiliary/Build
-vcvarsall.bat x64 
-set path=%path%;$GITHUB_WORKSPACE/package/Qt_Static-6.6.0/bin
-cd $GITHUB_WORKSPACE
-qmake -v
-qmake ./installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o ./build_output
