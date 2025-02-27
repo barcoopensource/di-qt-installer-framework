2e3d3d30488909cbb28a61d5ebb2964233edeec1
JasonSun0593 <srm2020@126.com>
2025-02-27 16:43:58 +0800

    chore: try merging two jobs into one for different jobs may not share same VM

--- scripts/installer-build.bat
+++ scripts/installer-build.bat
@@ -8,10 +8,11 @@ set path=%path%;%~dp0..\package\Qt_Static-6.6.0\bin
 set path=%path%;%~dp0..\package\Jom;
 
 REM qmake
-%~dp0..\package\Qt_Static-6.6.0\bin\qmake.exe .\installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o %~dp0..\build_output
+qmake -v
+qmake .\installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o %~dp0..\build_output
 
 REM jom
 cd build_output
-%~dp0..\package\Jom\jom.exe qmake_all
-%~dp0..\package\Jom\jom.exe -j4 -f Makefile
+jom.exe qmake_all
+jom.exe -j4 -f Makefile
 EndLocal
