987fb2546ad4b396228bfbab813002196659c70e
JasonSun0593 <srm2020@126.com>
2025-02-27 16:16:02 +0800

    chore: change to "CALL" vcvarsall to config env

--- scripts/installer-build.bat
+++ scripts/installer-build.bat
@@ -1,17 +1,18 @@
 @echo on
-
+SetLocal EnableDelayedExpansion
 REM set msvc env
-cd /d "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
-vcvarsall.bat x64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 REM set env path
-set path=%path%;%~dp0\..\package\Qt_Static-6.6.0\bin
-set path=%path%;%~dp0\..\package\Jom;
+set path=%path%;%~dp0..\package\Qt_Static-6.6.0\bin
+set path=%path%;%~dp0..\package\Jom;
 
 REM qmake
-cd /d "%~dp0\..\build_output"
-qmake ..\installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o %~dp0\..\build_output
+qmake -v
+qmake .\installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o %~dp0..\build_output
 
 REM jom
+cd build_output
 jom.exe qmake_all
 jom.exe -j4 -f Makefile
+EndLocal
