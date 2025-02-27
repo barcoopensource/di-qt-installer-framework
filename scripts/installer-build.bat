bb1c4fe787507737453eb79e8e1d199a7b50da7e
JasonSun0593 <srm2020@126.com>
2025-02-27 15:22:30 +0800

    chore: use bat and change echo to "on"

--- scripts/installer-build.bat
+++ scripts/installer-build.bat
@@ -1,15 +1,15 @@
-@echo off
+@echo on
 
 REM set msvc env
 cd /d "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 vcvarsall.bat x64
 
 REM set env path
-set path=%path%;%~dp0..\package\Qt_Static-6.6.0\bin
-set path=%path%;%~dp0..\package\Jom;
+set path=%path%;%~dp0\..\package\Qt_Static-6.6.0\bin
+set path=%path%;%~dp0\..\package\Jom;
 
 REM qmake
-cd /d "%~dp0..\build_output"
+cd /d "%~dp0\..\build_output"
 qmake ..\installerfw.pro -spec win32-msvc "CONFIG+=qtquickcompiler"  -o %~dp0\..\build_output
 
 REM jom
