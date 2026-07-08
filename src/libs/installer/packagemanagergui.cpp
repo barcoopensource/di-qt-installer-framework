3134178a20333da35a5eac8cca035a5fab0dfb92
wei-song_Barco <wei.song@barco.com>
2026-07-08 11:25:27 +0800

    fix: delete line and word warp mode setting

--- src/libs/installer/packagemanagergui.cpp
+++ src/libs/installer/packagemanagergui.cpp
@@ -2544,8 +2544,6 @@ LicenseAgreementPage::LicenseAgreementPage(PackageManagerCore *core)
     m_textBrowser->setOpenLinks(false);
     m_textBrowser->setOpenExternalLinks(true);
     m_textBrowser->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    m_textBrowser->setLineWrapMode(QTextEdit::WidgetWidth);
-    m_textBrowser->setWordWrapMode(QTextOption::WrapAtWordBoundaryOrAnywhere);
     m_textBrowser->setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
     m_textBrowser->setObjectName(QLatin1String("LicenseTextBrowser"));
     connect(m_textBrowser, &QTextBrowser::anchorClicked, this, &LicenseAgreementPage::openLicenseUrl);
